d0c7f53020f3e905ca738f9ff48ad80cd0a14089
Christos-Michail Faltsetas <32649648+chrfa37@users.noreply.github.com>
2017-11-17 07:44:30 +0200

    Add files via upload

---
+++ TestMain.java
@@ -0,0 +1,11 @@
+package Javelas;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		System.out.println("Distance is: " + WordDistance.getMinimumDistance("gumbo", "gambol"));
+		System.out.println("");
+
+	}
+
+}
