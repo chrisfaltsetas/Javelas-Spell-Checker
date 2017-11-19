3ebf505440d3a3937a60d1e7d33eef4e0309ea4c
aftername <31492758+aftername@users.noreply.github.com>
2017-11-20 01:18:40 +0200

    basics about hashmaps

---
+++ HashMapUses.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class HashMapUses {
+
+  public static void main(String args[]){
+     HashMap<Integer,String> hashmap =new HashMap<Integer,String>();
+     hashmap.put(1,"spyros lavranos");
+	 hashmap.put(2,"lion");
+	 hashmap.put(5,"ARGHH ..PIRATE");
+	 hashmap.put(54,"frogen says aloha");
+	hashmap.put(12,"Agar.io");
+     //how to get size of map  hashmap.size();
+   int   size=hashmap.size();
+     System.out.printf("size of map is %d \n", size);
+     //display hashmap elements hmap
+     System.out.println("HashMaps element\n" + hashmap);
+       //how to remove all mapping;
+       //hmap.clear()
+       hashmap.clear();
+         System.out.println("HashMaps element\n" + hashmap);
+	 }
+ }
---
+++ HashMapsBaseThings.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class HashMapsBaseThings {
+	public static void main(String args[]) {
+		/* declare Hashmap object*/
+		HashMap<Integer,String> hashmap = new HashMap<Integer,String>();
+		/* add elements  hashmap.put(Integer,String) (integer=key and String=value)*/
+		hashmap.put(1,"spyros lavranos");
+		hashmap.put(2,"lion");
+		hashmap.put(5,"ARGHH ..PIRATE");
+		hashmap.put(54,"frogen says aloha");
+		hashmap.put(12,"Agar.io");
+		//i just added 5 elements
+		//how get values  based on the key ;
+		//use hashmap.get(key)
+	String valueOfMap =hashmap.get(54);
+	System.out.println(valueOfMap);
+		//remove values based on key
+		//use hashmap.remove(key);
+		hashmap.remove(1);
+	}
+}
+
+
