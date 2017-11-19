e567b17c79a5c66490f5d2b5efc949fee573264a
Chris <32649648+chrfa37@users.noreply.github.com>
2017-11-19 14:50:44 +0200

    WordDistance class

--- WordDistance.java
+++ WordDistance.java
@@ -1,26 +1,77 @@
-package Javelas;
-
 public class WordDistance {
 	
 	public static int getMinimumDistance(String word1, String word2) {
 		
+		if (word1.length() < word2.length()) {
+			String temp = word1;
+			word1 = word2;
+			word2 = temp;			
+		}			
+		
 		int length1 = word1.length();
 		int length2 = word2.length();
 		
-		int[][] distanceMap = new int[length1 + 1][length2 + 1];
+		int[][] distanceMap = new int[length2 + 1][length1 + 1];
 		
-		for (int i = 0; i <= length1; i++) {
+		for (int i = 0; i <= length2; i++) {
 			distanceMap[i][0] = i;
 		}
 		
-		for (int i = 0; i <= length2; i++) {
+		for (int i = 0; i <= length1; i++) {
 			distanceMap[0][i] = i;
 		}
 		
-		for (int i = 0; i < length1; i++) {
-			char char1 = word1.charAt(i);
-			for (int j = 0; j < length2; j++) {
-				char char2 = word2.charAt(j);
+		for (int i = 0; i < length2; i++) {
+			char char2 = word2.charAt(i);
+			for (int j = 0; j < length1; j++) {
+				char char1 = word1.charAt(j);
+				
+				int topleft = distanceMap[i][j];
+				int top = distanceMap[i][j + 1];
+				int left = distanceMap[i + 1][j];
+				
+				distanceMap[i + 1][j + 1] = Math.min(topleft, Math.min(top, left));
+
+				if (char1 != char2)
+					distanceMap[i + 1][j + 1] ++;				
+			}//end for
+		}//end for
+		
+		//Test
+		printDistanceMap(distanceMap, word1, word2);
+		
+		return distanceMap[length2][length1];
+		
+	}//end method getMinimumDistance
+	
+	
+	public static int distanceOptimal(String word1, String word2) {
+		
+		if (word1.length() < word2.length()) {
+			String temp = word1;
+			word1 = word2;
+			word2 = temp;			
+		}			
+		
+		int length1 = word1.length();
+		int length2 = word2.length();
+		
+		int[][] distanceMap = new int[length2 + 1][length1 + 1];
+		
+		for (int i = 0; i <= length2; i++) {
+			distanceMap[i][0] = i;
+		}
+		
+		for (int i = 0; i <= length1; i++) {
+			distanceMap[0][i] = i;
+		}
+		
+
+			
+		for (int j = 0; j < length1; j++) {
+			char char1 = word1.charAt(j);								
+			for (int i = 0; i < ((j+1) < length2 ? (j+1) : length2); i++) {
+				char char2 = word2.charAt(i);
 				
 				int topleft = distanceMap[i][j];
 				int top = distanceMap[i][j + 1];
@@ -30,29 +81,36 @@ public class WordDistance {
 
 				if (char1 != char2)
 					distanceMap[i + 1][j + 1] ++;		
-			}//end for
-		}//end for
+				//printDistanceMap(distanceMap, word1, word2);
+			}
+		}
 		
-		//printDistanceMap(distanceMap);//Test
 		
-		return distanceMap[length1][length2];
+		//Test
+		printDistanceMap(distanceMap, word1, word2);
 		
-	}//end method getMinimumDistance
-
-	public static int findMinimun(int a, int b, int c) {
-		int min = a;
-		if (b < min)
-			min = b;
-		if (c < min)
-			min = c;
-		return min;
-	}
-
+		return distanceMap[length2][length1];
+		
+	}//end method distanceOptimal
+		
+	
 	//Test method
-	public static void printDistanceMap(int[][] distanceMap) {
+	public static void printDistanceMap(int[][] distanceMap, String word1, String word2) {
 		
-		for (int j = 0; j < distanceMap[0].length; j++) {
-			for (int i = 0; i < distanceMap.length; i++) {
+		word1 = word1.toUpperCase();
+		word2 = word2.toUpperCase();
+				
+		System.out.print("    ");
+		for (int i = 0; i < word1.length(); i++)
+			System.out.print(word1.charAt(i) + " ");
+		System.out.println();
+		
+		for (int i = 0; i < distanceMap.length; i++) {
+			if (i != 0)			
+				System.out.print(word2.charAt(i - 1) + " ");
+			else
+				System.out.print("  ");
+			for (int j = 0; j < distanceMap[0].length; j++) {
 				System.out.print(distanceMap[i][j] + " ");
 			}
 			System.out.println("");
@@ -60,4 +118,4 @@ public class WordDistance {
 		System.out.println("");
 	}
 	
-}
+}//end class
