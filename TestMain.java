2a93254f57ab717146d8b937da9a402a3c36a2a7
Chris <32649648+chrfa37@users.noreply.github.com>
2017-11-20 09:35:17 +0200

    Testing class

--- TestMain.java
+++ TestMain.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -11,6 +13,20 @@ public class TestMain {
 		System.out.println("");
 		System.out.println("");
 		System.out.println("Distance is: " + WordDistance.getMinimumDistance("sitting", "kitten"));	
+		
+				
+		HashMapsExample.HashMapsTest();
+		
+		String path = "C:/Users/CHRIS/Desktop/LAB/Java/Javelas/src/Greek.txt";//Needs to be changed
+		String encoding = "UTF8";
+		while(true) {
+			try {
+				SpellChecker.run(path, encoding);
+			} catch (IOException ioException) {
+				System.out.println (ioException.toString());
+				System.out.println("Could not find file: " + path);
+			}
+		}
 	}
 
 }
