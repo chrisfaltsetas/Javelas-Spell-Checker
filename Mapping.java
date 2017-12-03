df7bda15d11e9b640c0fffd437a7aedbf05ab7a9
Chris <32649648+chrfa37@users.noreply.github.com>
2017-12-03 21:17:52 +0200

    update

--- Mapping.java
+++ Mapping.java
@@ -61,10 +61,13 @@ public class Mapping {
 						wordExists = true;
 				}
 			}
+			
 			if (wordExists)
 				System.out.println(word + " exists");
-			else
+			else {
 				System.out.println(word + " doesn't exist");
+				firstSuggestions(word);
+			}
 		}
 	}
 
@@ -86,6 +89,39 @@ public class Mapping {
 		}
 		return jhash;
 	}
+	
+	
+	public static void firstSuggestions(String word) {
+		
+		word = word.substring(0, 1).toLowerCase() + word.substring(1).toLowerCase();
+		
+		boolean wordExists = false;
+		if (map.containsKey(jHashCode(word))) {
+			for (String wordInList: map.get(jHashCode(word))) {
+				if (wordInList.equals(word))
+					wordExists = true;
+			}
+		}
+		
+		if (wordExists)
+			System.out.println("Suggestion: " + word);
+		
+		
+		
+		word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+		
+		wordExists = false;
+		if (map.containsKey(jHashCode(word))) {
+			for (String wordInList: map.get(jHashCode(word))) {
+				if (wordInList.equals(word))
+					wordExists = true;
+			}
+		}
+		
+		if (wordExists)
+			System.out.println("Suggestion: " + word);
+		
+	}	
 
 
 }
