b65eba7ced3c63fad07ed03fd22a97db5e46872f
Christos-Michail Faltsetas <32649648+chrfa37@users.noreply.github.com>
2017-12-08 16:47:51 +0200

    Update and rename Mapping.java to Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
 
-public class Mapping {
+public class Dictionary {
 	
 	private static Scanner input;
 	private static BufferedReader reader;
