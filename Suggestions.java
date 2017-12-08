a86666b815ef3649e5420f2719a82db07fc4865b
spyros <spyros.lavranos5@gmail.com>
2017-12-09 01:02:09 +0200

    Update Suggestions.java

--- Suggestions.java
+++ Suggestions.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Suggestions {
+public class Suggestions extends WordEdit {
 	
 	private String word;
 	private ArrayList<String> suggestedWords;
@@ -10,7 +10,7 @@ public class Suggestions {
 		this.suggestedWords = new ArrayList<String>();
 	}
 		
-	public void run() {
+	public void getSuggestions() {
 		firstSuggestions();
 	}
 	
@@ -19,8 +19,8 @@ public class Suggestions {
 		word = word.substring(0, 1).toLowerCase() + word.substring(1).toLowerCase();
 		
 		boolean wordExists = false;
-		if (Dictionary.map.containsKey(Dictionary.jHashCode(word))) {
-			for (String wordInList: Dictionary.map.get(Dictionary.jHashCode(word))) {
+		if (grDic.getDic().containsKey(jHashCode(word))) {
+			for (String wordInList: grDic.getDic().get(jHashCode(word))) {
 				if (wordInList.equals(word))
 					wordExists = true;
 			}
@@ -34,8 +34,8 @@ public class Suggestions {
 		word = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
 		
 		wordExists = false;
-		if (Dictionary.map.containsKey(Dictionary.jHashCode(word))) {
-			for (String wordInList: Dictionary.map.get(Dictionary.jHashCode(word))) {
+		if (Dictionary.dic.containsKey(jHashCode(word))) {
+			for (String wordInList: Dictionary.dic.get(jHashCode(word))) {
 				if (wordInList.equals(word))
 					wordExists = true;
 			}
@@ -53,6 +53,24 @@ public class Suggestions {
 
 	public void setWord(String word) {
 		this.word = word;
-	}	
+	}
+	
+	public void secondSuggestions() {
+		ArrayList<String> sug = new ArrayList<String>();
+        int x = Dictionary.jHashCode(this.word) - 23;
+        int size;
+        for (int counter = 0; counter < 48;counter++) {
+            sug = Dictionary.map.get(x);
+            size = sug.size();
+            if (size > 0) {
+                for (int i = 0;i < size;i++) {
+                    if(WordDistance.getMinimumDistance(getWord(), sug.get(i)) == 1) {
+                        suggestedWords.add(sug.get(i));
+                    }
+                }
+            }
+            x++;
+        }
+    }
 	
 }
