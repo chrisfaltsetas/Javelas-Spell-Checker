512b3ae54393828ebeb7c567246b88b687db4676
Tassos Athanassopoulos <Tassos.Athanassopoulos567@gmail.com>
2017-12-08 23:58:03 +0200

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -4,90 +4,75 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.Scanner;
 
-public class Dictionary {
+public class Dictionary extends WordEdit {
 	
-	private static Scanner input;
-	private static BufferedReader reader;
-	static LinkedHashMap<Integer, ArrayList<String>> map = new LinkedHashMap<Integer, ArrayList<String>>();	
+	private LinkedHashMap<Integer, ArrayList<String>> dic;
+	private String lang;
+	static ArrayList<Dictionary> dictionaries = new ArrayList<Dictionary>();
 
-	//Run the programm
-	public static void run() throws IOException {
-				
-		loadDic();
-		//System.out.println(map.size());//testing
-		System.out.print("Γράψε ένα κείμενο φίλε μου: ");
-	 	input = new Scanner(System.in);
-		String text = input.nextLine();
-		searchDic(text);
+	
+	public Dictionary(String lang) throws IOException {
+		this.dic = new LinkedHashMap<Integer, ArrayList<String>>();
+		setDic(lang);
+		this.lang = lang;
+		dictionaries.add(this);
 	}
-
+	
 	//Import the dictionary in a LinkedHashMap
-	public static void loadDic() throws IOException {
-				
-		reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:/Users/CHRIS/Desktop/LAB/Java/Javelas/src/Greek.txt"), "UTF8"));
-		String nextWord = reader.readLine();
-		nextWord = reader.readLine();//Throw first line away, firstline = 575133 (not a word)
+	private void setDic(String lang) throws IOException {
+		
+		BufferedReader rdr = new BufferedReader(new InputStreamReader(new FileInputStream(lang + ".txt"), "UTF8"));
+		String nextWord = rdr.readLine();
 		int nextWordHash = 0;
 		nextWordHash = jHashCode(nextWord);
-		//int counter = 0;
-		while (nextWord != null /*&& counter < 1000*/) {
-			if (map.containsKey(nextWordHash)) {
-				(map.get(nextWordHash)).add(nextWord);
+		while (nextWord != null) {
+			if (dic.containsKey(nextWordHash)) {
+				(dic.get(nextWordHash)).add(nextWord);
 			} else {
 				ArrayList<String> list = new ArrayList<String>();
 				list.add(nextWord);
-				map.put(nextWordHash, list);
+				dic.put(nextWordHash, list);
 			}
-			//System.out.println(nextWord);//testing
-			nextWord = reader.readLine();
+			nextWord = rdr.readLine();
 			nextWordHash = jHashCode(nextWord);
-			//counter++;
 		}
-		//System.out.println(map.size());//testing DO NOT TRY TO PRINT MAP (takes time ~3min, resource heavy)
+		rdr.close();
 	}
 
     //Check if a word exists in the dictionary
-	public static void searchDic(String text) {
-		for (String word: text.split(" ")) {
-			word = specialCharacters(word);
-			//word = word.substring(0, 1) + word.substring(1).toLowerCase();
+	public boolean wordExists(String word) {
+		//for (String word: text.split(" ")) {
+			//word = specialCharacters(word);
 	
 			boolean wordExists = false;
-			if (map.containsKey(jHashCode(word))) {
-				for (String wordInList: map.get(jHashCode(word))) {
-					if (wordInList.equals(word))
+			if (dic.containsKey(jHashCode(word))) {
+				for (String wordInList: dic.get(jHashCode(word))) {
+					if (wordInList.equals(word)) {
 						wordExists = true;
+					}
 				}
 			}
-			
-			if (!wordExists) {
-				System.out.println(word + " doesn't exist");
-				Suggestions sug = new Suggestions(word);
-				sug.run();
-			}			
-			else 
-				System.out.println(word + " exists");						
-		}
+			return wordExists;
 	}
 
 	//Ignore the special characters
-	public static String specialCharacters(String words) {
-		words = words.replaceAll("[\\P{InGreek}a-zA-Z]", "");
-		System.out.println(words);//testing
-		return words;
+	public String specialCharacters(String word) {
+		word = word.substring(0, word.length() - 1) + word.substring(word.length() - 1).replaceAll("[^a-zA-Z&&[\\P{In" + lang + "}a-zA-Z]]", "");
+		System.out.println(word);//testing
+		return word;
+	}	
+
+	public LinkedHashMap<Integer, ArrayList<String>> getDic() {
+		return dic;
 	}
-	
-	public static int jHashCode(String word) {
-		int jhash = 0;
-		if (word != null) {
-			if (word.length() > 0) {
-				for (int i = 0; i < word.length(); i++) {
-					jhash += word.substring(i, i+1).hashCode();
-				}
-			}
-		}
-		return jhash;
+
+	public String getDicName() {
+		return lang;
 	}
+
+	public void setDicName(String dicName) {
+		this.lang = dicName;
+	}
+
 }
