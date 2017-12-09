95f92640a6b43b785a30ee2f5c028fe68a58cddd
Tassos Athanassopoulos <Tassos.Athanassopoulos567@gmail.com>
2017-12-09 03:47:07 +0200

    SpellingTest Update

--- SpellingTest.java
+++ SpellingTest.java
@@ -1,31 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class SpellingTest {
 
 	public static void main(String[] args) throws IOException {
-		//menu();
-		checkSpellingTest();
+		
+		menu();
+		String text = null;
+		Scanner input = new Scanner(System.in, "UTF8");
+		
+		int choice = input.nextInt();
+		
+		if (choice == 1) {
+			Scanner input1 = new Scanner(System.in, "UTF8");
+			System.out.print("Write a text: ");
+			text = input1.nextLine();
+		} else if (choice == 2) {
+			BufferedReader rdr = new BufferedReader(new InputStreamReader(new FileInputStream("Test.txt"), "UTF8"));
+			String nextWord = rdr.readLine();
+		} 
+		
+		checkSpellingTest(text);
+		
 	}
 	
-	public static void checkSpellingTest() throws IOException {
+	public static void checkSpellingTest(String text) throws IOException {
 		
 		Dictionary grDic = new Dictionary("Greek");
-		System.out.print("Write a text: ");
-	 	Scanner input = new Scanner(System.in, "UTF8");
-		String word = input.next();
-		grDic.wordExists(word);
-		System.out.print("Want to try another text? (Y/N): ");		
-		input.close();
+		for (String word: text.split(" ")) {
+			
+			if (!grDic.wordExists(word)) {
+				System.out.println(word + " doesn't exist");
+			}
+		}
+		//System.out.print("Want to try another text? (Y/N): ");		
 		
 	}
 	
 	public static void menu() {
-		System.out.println("What do you want to do:\n"
+		System.out.print("What do you want to do:\n"
 				+ "1) Write text\n"
 				+ "2) Import .txt file\n"
 				+ "3) Import .doc file\n"
-				+ "4) Give URL\n");
+				+ "4) Give URL\n"
+				+ "Choice: ");
 		
 	}
 
