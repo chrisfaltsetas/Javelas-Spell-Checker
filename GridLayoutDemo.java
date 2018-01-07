578732f4f04e02f9f07838f6b833aba739d24a61
RubatiAggeliki <32688065+RubatiAggeliki@users.noreply.github.com>
2018-01-08 00:25:23 +0200

    Add files via upload

--- GridLayoutDemo.java
+++ GridLayoutDemo.java
@@ -3,9 +3,9 @@ import javax.swing.JButton;
 import java.awt.GridLayout;
 
 public class GridLayoutDemo {
-	public static void main(String[] args) {
+	public void showList() {
 		JFrame jf = new JFrame("Javelas Spelling");
-		jf.setBounds(800,400,1000,800);
+		jf.setBounds(200,100,250,200);
 		jf.setLayout(new GridLayout(3,1));
 		jf.add(new JButton("Skip this word"));
 		jf.add(new JButton("Skip this word every time"));
